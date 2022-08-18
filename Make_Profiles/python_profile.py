2c4933ed77dbfcbde2c9ceccfc9b1dbb318dd79a
xaviergarcia5 <107950750+xaviergarcia5@users.noreply.github.com>
2022-08-18 16:10:22 -0400

    Plotting updates
    Plumbing update, asc proj, general function for as des profiles, m to cm

--- Make_Profiles/python_profile.py
+++ Make_Profiles/python_profile.py
@@ -5,26 +5,29 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 
-def plot_with_dots(data_file, model_file, outname):
+def plot_with_dots(data_file, model_file, outname, data_label, model_label):
+	print("Plotting", outname)
 	# THE DREAM: CHANGE LABEL FONT SIZE, METERS --> CENTIMETERS
 	[xd, yd, zd, pd, qd, _, _] = np.loadtxt(data_file, unpack=True);  # read in data (black dots)
 	[xm, ym, zm, pm, qm, _, _] = np.loadtxt(model_file, unpack=True);  # read in model 
 	plt.figure(dpi=300, figsize=(12,7));
-	plt.plot(pd, zd, marker='.', color='black', linewidth=0, label="Desc Data");
-	plt.plot(pm, zm, marker='.', color='orange', linewidth=0, label="Desc Model, 1 km depth, Mw5.4");
+	zd=np.multiply(zd, 100)
+	zm=np.multiply(zm, 100) #unit cnversion into cm
+	plt.plot(pd, zd, marker='.', color='black', linewidth=0, label=data_label);
+	plt.plot(pm, zm, marker='.', color='orange', linewidth=0, label=model_label);
 	plt.xlabel('Distance (km)');
-	plt.ylabel('LOS (m)');
+	plt.ylabel('LOS (cm)');
 	plt.legend();
 	plt.grid(True);
 	plt.savefig(outname);
 	return;
 
-
 if __name__ == "__main__":
-	data_file = "descending_profile.txt"
-	model_file = "descending_model_profile.txt"
-	outname = "profile_for_desc.png"
-	plot_with_dots(data_file, model_file, outname);
-
-
-
+	asc_data_file = "ascending_profile.txt"
+	asc_model_file = "ascending_model_profile.txt"
+	asc_outname = "profile_for_asc.png"
+	plot_with_dots(asc_data_file, asc_model_file, asc_outname, data_label="Asc Data", model_label="Asc Model, 5.8 km depth, Mw5.3");
+	des_data_file = "descending_profile.txt"
+	des_model_file = "descending_model_profile.txt"
+	des_outname = "profile_for_desc.png"
+	plot_with_dots(des_data_file, des_model_file, des_outname, data_label="Desc Data", model_label="Desc Model, 5.8 km depth, Mw5.3");
